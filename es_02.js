0597a5414ffe8065b753d4c6320e899f9ccc92bc
Santambrogio-Lorenzo <santambrogio.lorenzo@studenti.salesianisesto.it>
2022-05-25 08:34:56 +0200

    fix code

--- es_02.js
+++ es_02.js
@@ -1,57 +1,51 @@
-/*var count = 2;
-      $("#btn1").click(calculation("+"));
-          function calculation(segno) {
-        count++;
-        var n1 = +$("#N1").val();
-        var n2 = +$("#N2").val();
+var count = 2;
+$("#btn1").click(function () {
+  calculation("+");
+});
+$("#btn2").click(function () {
+  calculation("-");
+});
+$("#btn3").click(function () {
+  calculation("*");
+});
+$("#btn4").click(function () {
+  calculation("/");
+});
+
+function calculation(sign) {
+  if ($("#N1").val() && $("#N2").val()) {
+    count++;
+    var n1 = +$("#N1").val();
+    var n2 = +$("#N2").val();
+    var res = n1 + n2;
+    switch (sign) {
+      case "+":
         var res = n1 + n2;
-        switch(segno){
-            case "+":
-             var res = n1 + n2;
-             break;
-             case "-":
-                 var res = n1 - n2;
-                 break;
-                 case "/":
-                     var res = n1 / n2;
-                     break;
-                     case "*":
-                         var res = n1 * n2;
-                         break;
-     
-                        }
+        // n1 -(-n2) ;
+        break;
+      case "-":
+        var res = n1 - n2;
+        break;
+      case "*":
+        var res = n1 * n2;
+        break;
+      case "/":
+        var res = n1 / n2;
+        break;
     }
-    */
-    var count=3;
-    $("#btn1").click(function(){
-        var n1= +$("#N1").val();
-        var n2= +$("#N2").val();
-        var res=n1+n2;
-        console.log (res);
-        $("#table").append("<tr><td>"+count+"</td><td>"+n1+"</td><td>+</td><td>"+n2+"</td><td>"+res+"</td></tr>");
-        count ++;
-    });
-    $("#btn2").click(function(){
-        var n1=$("#N1").val();
-        var n2=$("#N2").val();
-        var res=n1-n2;
-        console.log (res);
-        $("#table").append("<tr><td>"+count+"</td><td>"+n1+"</td><td>-</td><td>"+n2+"</td><td>"+res+"</td></tr>");
-        count ++;
-    });
-    $("#btn3").click(function(){
-        var n1=$("#N1").val();
-        var n2=$("#N2").val();
-        var res=n1*n2;
-        console.log (res);
-        $("#table").append("<tr><td>"+count+"</td><td>"+n1+"</td><td>*</td><td>"+n2+"</td><td>"+res+"</td></tr>");
-        count ++;
-    });
-    $("#btn4").click(function(){
-        var n1=$("#N1").val();
-        var n2=$("#N2").val();
-        var res=n1/n2;
-        console.log (res);
-        $("#table").append("<tr><td>"+count+"</td><td>"+n1+"</td><td>/</td><td>"+n2+"</td><td>"+res+"</td></tr>");
-        count ++;
-    });
+    console.log(res);
+    $("#table").append(
+      "<tr><td>" +
+        count +
+        "</td><td>" +
+        n1 +
+        "</td><td>" +
+        sign +
+        "</td><td>" +
+        n2 +
+        "</td><td>" +
+        res +
+        "</td></tr>"
+    );
+  } else alert("fill all the text field");
+}
